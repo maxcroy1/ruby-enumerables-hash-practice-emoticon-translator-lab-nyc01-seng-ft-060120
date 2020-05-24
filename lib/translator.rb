d620638d54eff2881903fff1ccbcac57d7a55a41
Maxwell Croy <max.croy1@gmail.com>
2020-05-24 13:02:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,9 +22,9 @@ def get_japanese_emoticon(path, emoticon)
   dictionary.each do |key, value|
     if dictionary[key][:english] == emoticon
       translation = dictionary[key][:japanese]
-    else
-      translation = "Sorry, that emoticon was not found"
     end
+    if translation == ""
+      translation = "Sorry, that emoticon was not found"
   end
   translation
 end
